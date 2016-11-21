47a3a317729538f2d3f8e6ebc839a760ded158e3
Marcin Stanek <stanek.marcinp@gmail.com>
2016-11-21 02:18:09 +0100

    Update & Delete updates

--- src/ToDoApi/Controllers/TodoController.cs
+++ src/ToDoApi/Controllers/TodoController.cs
@@ -30,12 +30,53 @@ namespace ToDoApi.Controllers
         public IActionResult GetById(string id)
         {
             var item = TodoItems.Find(id);
-            if(item == null)
+            if (item == null)
             {
                 return NotFound();
             }
             return new ObjectResult(item);
         }
 
+        [HttpPost]
+        public IActionResult Create([FromBody] TodoItem item)
+        {
+            if (item == null)
+            {
+                return BadRequest();
+            }
+            TodoItems.Add(item);
+            return CreatedAtRoute("GetTodo", new {id = item.Key}, item);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(string id, [FromBody] TodoItem item)
+        {
+            if (item == null || item.Key != id)
+            {
+                return BadRequest();
+            }
+
+            var todo = TodoItems.Find(id);
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            TodoItems.Update(item);
+            return new NoContentResult();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(string id)
+        {
+            var todo = TodoItems.Find(id);
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            TodoItems.Remove(id);
+            return new NoContentResult();
+        }
     }
 }
