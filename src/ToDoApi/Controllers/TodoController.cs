11fa6819897305b45fbca568b94833ad291dec0e
Marcin Stanek <stanek.marcinp@gmail.com>
2016-11-21 01:13:40 +0100

    Controllers for Todo

---
+++ src/ToDoApi/Controllers/TodoController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ToDoApi.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using ToDoApi.Models;
+
+
+namespace ToDoApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class TodoController : Controller
+    {
+        public TodoController(ITodoRepository todoItems)
+        {
+            TodoItems = todoItems;
+        }
+
+        public ITodoRepository TodoItems { get; private set; }
+
+        [HttpGet]
+        public IEnumerable<TodoItem> GetAll()
+        {
+            return TodoItems.GetAll();
+        }
+
+
+        [HttpGet("{id}", Name = "GetTodo")]
+        public IActionResult GetById(string id)
+        {
+            var item = TodoItems.Find(id);
+            if(item == null)
+            {
+                return NotFound();
+            }
+            return new ObjectResult(item);
+        }
+
+    }
+}
